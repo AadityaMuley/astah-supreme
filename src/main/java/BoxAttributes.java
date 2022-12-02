86412f800fe9d8cf440e3b23839c253c8d40321b
Aaditya Muley <aadityamuley8190@gmail.com>
2022-12-01 20:39:29 -0700

    Status bar set with observer pattern

--- src/main/java/BoxAttributes.java
+++ src/main/java/BoxAttributes.java
@@ -1,7 +1,8 @@
 import java.awt.*;
 import java.awt.geom.Line2D;
+import java.util.Observable;
 
-public class BoxAttributes {
+public class BoxAttributes extends Observable {
     public int isBox = 1; // used in both constructors and determines if current object stores data for a rectangle or a relationship line
                           // 0-relationship line, 1-rectangle
     public String name;
@@ -10,18 +11,47 @@ public class BoxAttributes {
     public String start;
     public String end;
     public Line2D line;
+    public String[] status = new String[2];
 
-    public BoxAttributes(int isBox, String name, Rectangle rectangle) {
+//    public BoxAttributes(int isBox, String name, Rectangle rectangle) {
+//        this.isBox = isBox;
+//        this.name = name;
+//        this.rectangle = rectangle;
+//    }
+//
+//    public BoxAttributes(int isBox, int relationship, String start, String end, Line2D line) {
+//        this.isBox = isBox;
+//        this.relationship = relationship;
+//        this.start = start;
+//        this.end = end;
+//        this.line = line;
+//    }
+    
+    public void setBoxAttributes(int isBox, String name, Rectangle rectangle) {
         this.isBox = isBox;
         this.name = name;
         this.rectangle = rectangle;
+        status[0] = String.valueOf(isBox);
+        status[1] = name;
+        
+        setChanged();
+        notifyObservers();
     }
     
-    public BoxAttributes(int isBox, int relationship, String start, String end, Line2D line) {
+    public void setLineAttributes(int isBox, int relationship, String start, String end, Line2D line) {
         this.isBox = isBox;
         this.relationship = relationship;
         this.start = start;
         this.end = end;
         this.line = line;
+        status[0] = String.valueOf(isBox);
+        status[1] = String.valueOf(relationship);
+        
+        setChanged();
+        notifyObservers();
+    }
+    
+    public String[] getStatus() {
+        return(status);
     }
 }
